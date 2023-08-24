458e94c66381dd2482f1c99551990a333a792eda
Jorge Lobo <jorge-lobo@outlook.pt>
2023-08-24 19:24:51 +0100

    actualização

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // fixar footer quando a altura da página é mínima. libertar quando há necessidade de scroll
-window.addEventListener('DOMContentLoaded', function() {
+/* window.addEventListener('DOMContentLoaded', function () {
     const contentWrapper = document.querySelector('.content_wrapper');
     const footerSection = document.querySelector('.footer_section');
 
@@ -13,5 +13,34 @@ window.addEventListener('DOMContentLoaded', function() {
 
     window.addEventListener('resize', updateFooterPosition);
     updateFooterPosition();
-});
+}); */
 
+//mostrar imagem escolhida
+document.addEventListener('DOMContentLoaded', function () {
+    const productImageInput = document.getElementById('product_image');
+    const imagePreview = document.getElementById('image_preview');
+    const defaultImage = document.getElementById('default_image');
+
+    productImageInput.addEventListener('change', function () {
+
+        if (productImageInput.files && productImageInput.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                if (defaultImage) {
+                    defaultImage.style.display = 'none';
+                }
+                const imageElement = document.createElement('img');
+                imageElement.src = e.target.result;
+                imageElement.style.maxWidth = '100%';
+                imagePreview.innerHTML = '';
+                imagePreview.appendChild(imageElement);
+            };
+
+            reader.readAsDataURL(productImageInput.files[0]);
+        }
+    });
+    if (imagePreview.innerHTML.trim() === '') {
+        defaultImage.style.display = 'block';
+    }
+});
